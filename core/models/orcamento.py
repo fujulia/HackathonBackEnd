f8f7e3c6bfec1e594721c7cef56cf69a94fa1299
julia <juliafu713@gmail.com>
2024-10-31 17:07:47 -0300

    FEAT: Model do orçamento #9

---
+++ core/models/orcamento.py
@@ -0,0 +1,20 @@
+from django.db import models 
+from .user import User
+from django.utils import timezone
+
+class Orcamento(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compra", null=True, blank=True)
+    consumoMensal = models.DecimalField(max_digits=10, decimal_places=2)
+    gastoSemPlaca = models.DecimalField(max_digits=10, decimal_places=2)
+    estado = models.CharField(max_length=255, null=False, blank=True)
+    irradiacao = models.DecimalField(max_digits=10, decimal_places=2)
+    potenciaPlaca = models. DecimalField(max_digits=10, decimal_places=5)
+    areaDisponivel = models.DecimalField(max_digits=10, decimal_places=2)
+    gastoComPlaca = models.DecimalField(max_digits=10, decimal_places=2)
+    playback = models.DecimalField(max_digits=10, decimal_places=1)
+    porcetangemReducao = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateTimeField(default=timezone.now)
+     
+     
+    def __str__(self):
+       return f'({self.usuario}) {self.data}'
